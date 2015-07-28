f481e18055f84b7055e25c6516661a54ff1706f9
Kasey McCormick <mccormick.kasey@gmail.com>
2015-07-27 20:01:51 -0500

    initial commit (changing computers)

---
+++ app.rb
@@ -0,0 +1,8 @@
+=begin
+Write a program that starts with an array of numbers.
+Ten it finds the biggest number in the array and doubles it.
+=end
+array = [21,42,12,5]
+biggest = array.max
+puts biggest * biggest
+
